7d7c33c0570c3aac80d5b0375b21793bc86cdbc9
chamath silva <chamath.silva@syscolabs.com>
2021-09-17 16:01:05 +0530

    add : date picker

--- src/main.js
+++ src/main.js
@@ -1,73 +1,74 @@
-document.getElementById("login").onclick = function () {
-  window.location.href = "./createappoinment.html";
-};
-
-// document.getElementById("appointmentSubmit").onclick = function () {
-//   window.location.href = "./thankyou.html";
-//   console.log("aaaaaaaaafjafa")
+// document.getElementById("login").onclick = function () {
+//   window.location.href = "createappoinment.html";
 // };
 
-// document.getElementById("createAppoinment").onclick = function () {
-//   window.location.href = "./cancellations.html";
-//   console.log("afdgdsssssssa")
-// };
+$(document).ready(function () {
+  $("#login").click(function (e) {
+    e.preventDefault();
+    window.location.href = "createappoinment.html";
+  });
 
-$('.datepicker').datepicker({
-  startDate: '-3d'
+  $("#appointmentSubmit").click(function (e) {
+    e.preventDefault();
+    window.location.href = "thankyou.html";
+  });
+
+
+  $(".datepicker1").datepicker({todayHighlight : true, title: 'Date of Birth'});
+
+  $('.input-daterange').datepicker({todayHighlight : true});
+
+  $('.input-group.date').datepicker({});
 });
 
 
+
 $(function () {
-  'use strict';
-  $('.menu').hide();
-  $('.navbar').removeClass('shadow-sm');
-  $('.navbar-brand').css("display", "block")
+  "use strict";
+  $(".menu").hide();
+  $(".navbar").removeClass("shadow-sm");
+  $(".navbar-brand").css("display", "block");
 
-  $('.hamburger-menu-wrapper').click(function (e) {
-      e.preventDefault();
+  $(".hamburger-menu-wrapper").click(function (e) {
+    e.preventDefault();
 
-      if ($(this).hasClass('active')) {
-          $(this).removeClass('active');
-          $('.menu-overlay').fadeToggle('fast', 'linear');
-          $('.menu').slideToggle('slow');
-      } else {
-          $(this).addClass('active');
-          $('.menu-overlay').fadeToggle('fast', 'linear');
-          $('.menu').slideToggle('slow');
-          $(this).addClass('active');
-
-      }
-  })
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(".menu-overlay").fadeToggle("fast", "linear");
+      $(".menu").slideToggle("slow");
+    } else {
+      $(this).addClass("active");
+      $(".menu-overlay").fadeToggle("fast", "linear");
+      $(".menu").slideToggle("slow");
+      $(this).addClass("active");
+    }
+  });
 
   // if (on_index == "home") {
   //     $('.navbar-brand').hide();
   // }
 
-  const $menu = $('.navbar');
+  const $menu = $(".navbar");
 
-  $(document).mouseup(e => {
-      if (!$menu.is(e.target)
-          && $menu.has(e.target).length === 0) {
-          $('.ham').removeClass('active');
-          $('.menu').hide();
-      }
+  $(document).mouseup((e) => {
+    if (!$menu.is(e.target) && $menu.has(e.target).length === 0) {
+      $(".ham").removeClass("active");
+      $(".menu").hide();
+    }
   });
 
   $(window).scroll(function () {
-      $('.ham').removeClass('active');
-      $('.menu').hide();
+    $(".ham").removeClass("active");
+    $(".menu").hide();
 
-      if ($(this).scrollTop() > 0) {
-          $('.navbar').addClass('shadow-sm');
-          $('.navbar-brand').show();
-      }
-      else {
-          $('.navbar').removeClass('shadow-sm');
-          // if (on_index == "home") {
-          //     $('.navbar-brand').hide();
-          // }
-      }
-
+    if ($(this).scrollTop() > 0) {
+      $(".navbar").addClass("shadow-sm");
+      $(".navbar-brand").show();
+    } else {
+      $(".navbar").removeClass("shadow-sm");
+      // if (on_index == "home") {
+      //     $('.navbar-brand').hide();
+      // }
+    }
   });
-
 });
